46c0df9d5fd8976a0c8cc5697b52b9b95d179b8b
m00nLeo <tranlehoanglong2001@gmail.com>
2024-01-30 15:31:00 +0700

    Updates

---
+++ precache-manifest.1e8b997f8dbdd8f40bd1f953217cd38c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0637fbce064520b63b35d6ff859c7a",
+    "url": "/TheMovieRewind/index.html"
+  },
+  {
+    "revision": "6f6b939a133fa54575b0",
+    "url": "/TheMovieRewind/static/css/2.d00cefaa.chunk.css"
+  },
+  {
+    "revision": "4d6b68d964eb023fee47",
+    "url": "/TheMovieRewind/static/css/main.e305fa15.chunk.css"
+  },
+  {
+    "revision": "6f6b939a133fa54575b0",
+    "url": "/TheMovieRewind/static/js/2.283aa4fc.chunk.js"
+  },
+  {
+    "revision": "89382ab466df4e5acc824f9f27148885",
+    "url": "/TheMovieRewind/static/js/2.283aa4fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d6b68d964eb023fee47",
+    "url": "/TheMovieRewind/static/js/main.1a069000.chunk.js"
+  },
+  {
+    "revision": "6492fb4e7ddf97fec4b4",
+    "url": "/TheMovieRewind/static/js/runtime-main.aa1b6e00.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/TheMovieRewind/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/TheMovieRewind/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/TheMovieRewind/static/media/revicons.2feb69cc.eot"
+  }
+]);
